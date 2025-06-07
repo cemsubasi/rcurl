c259174a01b315c9091d4f8b9adc4c4e347bab9f
Cem Subaşı <cemsubasi@live.com>
2025-06-07 17:55:17 +0300

    Refactor[Program]: The way of parsing

--- Program.cs
+++ Program.cs
@@ -1,52 +1,68 @@
 ﻿using System.Diagnostics;
 
-var web = new { remote = "https://lp-web-dev-app.logipoly.com", local = "http://localhost:5550" };
-var auth = new { remote = "https://lp-auth-dev-app.logipoly.com", local = "http://localhost:5540" };
-var mobile = new { remote = "https://lp-mobile-dev-app.logipoly.com", local = "http://localhost:5580" };
-var admin = new { remote = "https://lp-admin-dev.logipoly.com", local = "http://localhost:5560" };
+var remoteToLocal = new Dictionary<string, string>
+{
+    { "https://lp-web-dev-app.logipoly.com", "http://localhost:5550" },
+    { "https://lp-auth-dev-app.logipoly.com", "http://localhost:5540" },
+    { "https://lp-mobile-dev-app.logipoly.com", "http://localhost:5580" },
+    { "https://lp-admin-dev.logipoly.com", "http://localhost:5560" },
+};
+
+string ReplaceWithLocal(string input) {
+  foreach (var kvp in remoteToLocal) {
+    if (input.Contains(kvp.Key)) {
+      return input.Replace(kvp.Key, kvp.Value);
+    }
+  }
+  return input;
+}
 
 var input = args.ToList();
-/* var custom = new List<string>(); */
-input = input.Select(x => x.Contains("curl ") ? string.Empty : x).ToList();
-input = input.Select(x => x.Contains("https") ? $"{x}" : x).ToList();
-input = input.Select(x => x.Contains(web.remote) ? x.Replace(web.remote, web.local) : x.Contains(auth.remote) ? x.Replace(auth.remote, auth.local) : x.Contains(mobile.remote) ? x.Replace(mobile.remote, mobile.local) : x.Contains(admin.remote) ? x.Replace(admin.remote, admin.local) : x).ToList();
-/* custom.Add(input.First(x => x.Contains("http"))); */
-/* if (input.Any(x => x.StartsWith("{"))) { */
-/*   custom.Add($"--data-raw {input.First(x => x.StartsWith("{")).ToString()}"); */
-/* } */
 
-/* var index = 0; */
-/* while (!index.Equals(-1)) { */
-/*   Console.WriteLine(index); */
-/*   index = input.IndexOf("-H", index + 1); */
-/*   if (index.Equals(-1)) { */
-/*     continue; */
-/*   } */
+input = input.Where(x => x != "curl").ToList();
 
-/*   var nextIndex = input.IndexOf("-", index); */
-/*   if (!nextIndex.Equals(-1)) { */
-/*     custom.Add($"'{string.Join(' ', input.Where((x, i) => i >= index && i < nextIndex))}'"); */
-/*   } */
-/* } */
+input = input.Select(ReplaceWithLocal).ToList();
 
-/* if (input.Any(x => x.Equals("--jsonpp"))) { */
-/*   input = input.Select(x => x.Equals("--jsonpp") ? string.Empty : x).ToList(); */
-/* custom.Add(" |json_pp"); */
-/* } */
+string argsString = string.Join(" ", input.Select(arg =>
+    arg.Contains(' ') || arg.Contains('"') ? $"\"{arg.Replace("\"", "\\\"")}\"" : arg
+));
 
-/* custom.ForEach(Console.WriteLine); */
-/* input = input.Select(x => !x.StartsWith('-') ? '\'' + x + '\'' : x).ToList(); */
-input = input.Select(x => !x.StartsWith('-') ? '\'' + x + '\'' + " \\\n" : x).ToList();
-/* Console.WriteLine("curl " + string.Join(' ', input)); */
+var psi = new ProcessStartInfo {
+  FileName = "curl",
+  Arguments = argsString,
+  RedirectStandardOutput = true,
+  RedirectStandardError = true,
+  UseShellExecute = false
+};
 
-var psi = new ProcessStartInfo();
-psi.FileName = "curl";
-psi.Arguments = string.Join(' ', input);
-psi.RedirectStandardOutput = true;
-var process = Process.Start(psi);
-var result = process!.StandardOutput.ReadToEnd();
-Console.WriteLine(result);
+var process = Process.Start(psi)!;
+var output = process.StandardOutput.ReadToEnd();
+var error = process.StandardError.ReadToEnd();
+process.WaitForExit();
 
-System.Diagnostics.Process.Start(psi);
+Console.WriteLine(output);
+if (!string.IsNullOrEmpty(error))
+  Console.Error.WriteLine(error);
 
-/* Console.WriteLine(psi.Arguments); */
+var jqpsi = new ProcessStartInfo {
+  FileName = "jq",
+  Arguments = "-C .",
+  RedirectStandardInput = true,
+  RedirectStandardOutput = true,
+  RedirectStandardError = true,
+  UseShellExecute = false
+};
+
+var jqProcess = Process.Start(jqpsi)!;
+
+await jqProcess.StandardInput.WriteAsync(output);
+jqProcess.StandardInput.Close(); // stdin'i kapatmazsan jq beklemede kalabilir
+
+var jqOutput = await jqProcess.StandardOutput.ReadToEndAsync();
+var jqError = await jqProcess.StandardError.ReadToEndAsync();
+
+jqProcess.WaitForExit();
+
+Console.WriteLine(jqOutput);
+if (!string.IsNullOrEmpty(jqError))
+  Console.Error.WriteLine(jqError);
