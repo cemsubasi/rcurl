ad47418995cf9fa374ceaa21a66b4d3887f25302
Cem Subaşı <cemsubasi@live.com>
2025-06-08 06:14:43 +0300

    Fix: Release version of rcurl on formula

--- rcurl.rb
+++ rcurl.rb
@@ -1,7 +1,7 @@
 class Rcurl < Formula
   desc "rcurl - A CLI tool"
   homepage "https://github.com/cemsubasi/rcurl"
-  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.13/rcurl-osx-arm64.tar.gz"
+  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.14/rcurl-osx-arm64.tar.gz"
   sha256 "12d54c99898bc838a104ec1db703ed010df57aa032023c9fd2293044c1abfd81"
 
   def install
