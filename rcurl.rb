431580e8c88e5d6097d157c82e1b7cec52613203
Cem Subaşı <cemsubasi@live.com>
2025-06-14 21:37:13 +0300

    Refactor: Brew tap file

--- rcurl.rb
+++ rcurl.rb
@@ -1,8 +1,9 @@
 class Rcurl < Formula
   desc "rcurl - A CLI tool"
   homepage "https://github.com/cemsubasi/rcurl"
-  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.15/rcurl-osx-arm64.tar.gz"
-  sha256 "5bbe2c4a10703e9fd85b8c039ef946f05ae2f9a4e64455b8d97a629a6acbdae0"
+  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.16/rcurl-osx-arm64.tar.gz"
+  sha256 "b1e188e6057f5c39a947983958c3435559a4521ac21c39dee213574efcec14e7"
+  
 
   def install
     bin.install "rcurl"
