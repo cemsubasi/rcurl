8145ad063813940b456e0cc08025d0171f9b7d61
Cem Subaşı <cemsubasi@live.com>
2025-09-21 01:41:20 +0300

    Feat[rb]: Add sha checksum into formula

--- rcurl.rb
+++ rcurl.rb
@@ -1,8 +1,8 @@
 class Rcurl < Formula
   desc "rcurl - A CLI tool"
   homepage "https://github.com/cemsubasi/rcurl"
-  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.16/rcurl-osx-arm64.tar.gz"
-  sha256 "b1e188e6057f5c39a947983958c3435559a4521ac21c39dee213574efcec14e7"
+  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.17/rcurl-osx-arm64.tar.gz"
+  sha256 "e6be1cf8e6d8a3a21159517b2592243ed126036f24a665e32646b40cc95adbe7"
   
 
   def install
