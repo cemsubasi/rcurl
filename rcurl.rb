ef1a1f12efab5b4f80ea451c0cffdaee57987518
Cem Subaşı <cemsubasi@live.com>
2025-09-21 03:04:27 +0300

    Refactor: Amend

--- rcurl.rb
+++ rcurl.rb
@@ -1,7 +1,7 @@
 class Rcurl < Formula
   desc "rcurl - A CLI tool"
   homepage "https://github.com/cemsubasi/rcurl"
-  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.18/rcurl-osx-arm64.tar.gz"
+  url "https://github.com/cemsubasi/rcurl/releases/download/v1.0.19/rcurl-osx-arm64.tar.gz"
   sha256 "63837ada3d5412aa5bed8d9a6d630b2b66805a79d1e5e18def10f1a8cc4fae82"
   
 
